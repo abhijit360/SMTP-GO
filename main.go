0d186958e2d4d71f532dd3ec03d2dc291348c35f
abhijit <kamathabhijit2002@gmail.com>
2024-10-31 23:36:33 -0400

    added some improved functionality to golang project

--- main.go
+++ main.go
@@ -1,12 +1,28 @@
 package main
 
 import (
-	// "bufio"
+	"bufio"
 	"fmt"
 	"net"
 	"strings"
+	"io"
 )
 
+var connectionCodes = map[string]int{
+    "STATUS_OK":                    250,
+    "TRANSACTION_FAILED":            554,
+    "SERVICE_READY":                 220,
+    "SERVICE_CLOSING":               221,
+    "SERVICE_NOT_AVAILABLE":         421,
+    "MAILBOX_NOT_AVAILABLE":         450, // same for mailbox not available
+    "REQUESTED_ACTION_NOT_TAKEN":    550,
+}
+
+type email struct{
+	from string
+	to string
+	content string
+}
 
 func main(){
 	fmt.Println("hello from go")
@@ -22,24 +38,50 @@ func main(){
 			return
 		}
 		handleMailConnection(conn)
+		defer listener.Close()
 	}
 
 }
 
+func formatMessage(status_code int , message string) []byte {
+	return []byte(fmt.Sprintf("%d %s\r\n", status_code, message))
+}
+
 func handleMailConnection(conn net.Conn){
 	fmt.Printf("connecting from: %v\n",conn.RemoteAddr())
-	STATUS_OK := []byte("250 ok")
 	buffer := make([]byte,1024)
-	conn.Read(buffer)
+	_ , err := conn.Read(buffer)
+	if err != nil {
+		fmt.Println("Error reading from connection:",err)
+		return
+	}
 	split_strings := strings.Split(string(buffer)," ")
 	cmd := strings.ToLower(split_strings[0])
+	
+	currentEmail := email{"","",""}
 
 	switch(cmd){
 	case "helo":
-		// clear all the storage etc and return a 250 ok
-		conn.Write(STATUS_OK)
+		// clear all the storage etc and return a 250 ok		
+		conn.Write(formatMessage(connectionCodes["STATUS_OK"],"Ready to get email!"))
 	case "mail":
-		defer conn.Close()
+		domain := split_strings[1]
+		currentEmail.from = string(domain)
+
+		conn.Write(formatMessage(connectionCodes["STATUS_OK"],"ok"))
+	case "data":
+		reader := bufio.NewReader(conn)
+		for {
+			line, err := reader.ReadString("\n");
+			if err == io.EOF{
+				break
+			}
+			if strings.TrimSpace(line) == "."{
+				break
+			}
+			currentEmail.content = fmt.Sprintf("%v\n %v", currentEmail.content,line)
+		}
+		conn.Write(formatMessage(connectionCodes["STATUS_OK"], "Message received"))
 	}
 
 }	
--- readme.md
+++ readme.md
@@ -2,6 +2,9 @@
 
 This project is to better my understanding of protocols used for internet communication and get better at golang.
 
+SMTP documentation:
+- https://www.rfc-editor.org/rfc/rfc821.txt
+
 Testing documentation:
 - https://stackoverflow.com/questions/34653875/python-how-to-send-data-over-tcp
 - Use python to test my server
